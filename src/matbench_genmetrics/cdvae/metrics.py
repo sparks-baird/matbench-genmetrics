61fd55e47838c5642c6cf9d7140cf508eed3a543
sgbaird <sterling.baird@icloud.com>
2022-06-25 00:22:33 -0600

    cdvae metrics.py

---
+++ src/matbench_genmetrics/cdvae/metrics.py
@@ -0,0 +1,509 @@
+"""Modified from source: https://github.com/txie-93/cdvae, MIT License"""
+import itertools
+from collections import Counter
+
+import numpy as np
+import smact
+from matminer.featurizers.composition.composite import ElementProperty
+from matminer.featurizers.site.fingerprint import CrystalNNFingerprint
+from pymatgen.analysis.structure_matcher import StructureMatcher
+from pymatgen.core.composition import Composition
+from pymatgen.core.lattice import Lattice
+from pymatgen.core.structure import Structure
+from scipy.spatial.distance import cdist, pdist
+from scipy.stats import wasserstein_distance
+from smact.screening import pauling_test
+from tqdm import tqdm
+
+from matbench_genmetrics.cdvae.constants import CompScalerMeans, CompScalerStds
+
+Percentiles = {
+    "mp20": np.array([-3.17562208, -2.82196882, -2.52814761]),
+    "carbon": np.array([-154.527093, -154.45865733, -154.44206825]),
+    "perovskite": np.array([0.43924842, 0.61202443, 0.7364607]),
+}
+
+COV_Cutoffs = {
+    "mp20": {"struc": 0.4, "comp": 10.0},
+    "carbon": {"struc": 0.2, "comp": 4.0},
+    "perovskite": {"struc": 0.2, "comp": 4},
+}
+
+
+def get_fp_pdist(fp_array):
+    if isinstance(fp_array, list):
+        fp_array = np.array(fp_array)
+    fp_pdists = pdist(fp_array)
+    return fp_pdists.mean()
+
+
+def filter_fps(struc_fps, comp_fps):
+    assert len(struc_fps) == len(comp_fps)
+
+    filtered_struc_fps, filtered_comp_fps = [], []
+
+    for struc_fp, comp_fp in zip(struc_fps, comp_fps):
+        if struc_fp is not None and comp_fp is not None:
+            filtered_struc_fps.append(struc_fp)
+            filtered_comp_fps.append(comp_fp)
+    return filtered_struc_fps, filtered_comp_fps
+
+
+def compute_cov(crys, gt_crys, struc_cutoff, comp_cutoff, num_gen_crystals=None):
+    struc_fps = [c.struct_fp for c in crys]
+    comp_fps = [c.comp_fp for c in crys]
+    gt_struc_fps = [c.struct_fp for c in gt_crys]
+    gt_comp_fps = [c.comp_fp for c in gt_crys]
+
+    assert len(struc_fps) == len(comp_fps)
+    assert len(gt_struc_fps) == len(gt_comp_fps)
+
+    # Use number of crystal before filtering to compute COV
+    if num_gen_crystals is None:
+        num_gen_crystals = len(struc_fps)
+
+    struc_fps, comp_fps = filter_fps(struc_fps, comp_fps)
+
+    comp_fps = CompScaler.transform(comp_fps)
+    gt_comp_fps = CompScaler.transform(gt_comp_fps)
+
+    struc_fps = np.array(struc_fps)
+    gt_struc_fps = np.array(gt_struc_fps)
+    comp_fps = np.array(comp_fps)
+    gt_comp_fps = np.array(gt_comp_fps)
+
+    struc_pdist = cdist(struc_fps, gt_struc_fps)
+    comp_pdist = cdist(comp_fps, gt_comp_fps)
+
+    struc_recall_dist = struc_pdist.min(axis=0)
+    struc_precision_dist = struc_pdist.min(axis=1)
+    comp_recall_dist = comp_pdist.min(axis=0)
+    comp_precision_dist = comp_pdist.min(axis=1)
+
+    cov_recall = np.mean(
+        np.logical_and(
+            struc_recall_dist <= struc_cutoff, comp_recall_dist <= comp_cutoff
+        )
+    )
+    cov_precision = (
+        np.sum(
+            np.logical_and(
+                struc_precision_dist <= struc_cutoff, comp_precision_dist <= comp_cutoff
+            )
+        )
+        / num_gen_crystals
+    )
+
+    metrics_dict = {
+        "cov_recall": cov_recall,
+        "cov_precision": cov_precision,
+        "amsd_recall": np.mean(struc_recall_dist),
+        "amsd_precision": np.mean(struc_precision_dist),
+        "amcd_recall": np.mean(comp_recall_dist),
+        "amcd_precision": np.mean(comp_precision_dist),
+    }
+
+    combined_dist_dict = {
+        "struc_recall_dist": struc_recall_dist.tolist(),
+        "struc_precision_dist": struc_precision_dist.tolist(),
+        "comp_recall_dist": comp_recall_dist.tolist(),
+        "comp_precision_dist": comp_precision_dist.tolist(),
+    }
+
+    return metrics_dict, combined_dist_dict
+
+
+class StandardScaler:
+    """A :class:`StandardScaler` normalizes the features of a dataset.
+    When it is fit on a dataset, the :class:`StandardScaler` learns the
+        mean and standard deviation across the 0th axis.
+    When transforming a dataset, the :class:`StandardScaler` subtracts the
+        means and divides by the standard deviations.
+    """
+
+    def __init__(self, means=None, stds=None, replace_nan_token=None):
+        """
+        :param means: An optional 1D numpy array of precomputed means.
+        :param stds: An optional 1D numpy array of precomputed standard deviations.
+        :param replace_nan_token: A token to use to replace NaN entries in the features.
+        """
+        self.means = means
+        self.stds = stds
+        self.replace_nan_token = replace_nan_token
+
+    def fit(self, X):
+        """
+        Learns means and standard deviations across the 0th axis of the data :code:`X`.
+        :param X: A list of lists of floats (or None).
+        :return: The fitted :class:`StandardScaler` (self).
+        """
+        X = np.array(X).astype(float)
+        self.means = np.nanmean(X, axis=0)
+        self.stds = np.nanstd(X, axis=0)
+        self.means = np.where(
+            np.isnan(self.means), np.zeros(self.means.shape), self.means
+        )
+        self.stds = np.where(np.isnan(self.stds), np.ones(self.stds.shape), self.stds)
+        self.stds = np.where(self.stds == 0, np.ones(self.stds.shape), self.stds)
+
+        return self
+
+    def transform(self, X):
+        """
+        Transforms the data by subtracting the means and dividing by the standard
+        deviations.
+        :param X: A list of lists of floats (or None).
+        :return: The transformed data with NaNs replaced by
+        :code:`self.replace_nan_token`.
+        """
+        X = np.array(X).astype(float)
+        transformed_with_nan = (X - self.means) / self.stds
+        transformed_with_none = np.where(
+            np.isnan(transformed_with_nan), self.replace_nan_token, transformed_with_nan
+        )
+
+        return transformed_with_none
+
+    def inverse_transform(self, X):
+        """
+        Performs the inverse transformation by multiplying by the standard deviations
+        and adding the means.
+        :param X: A list of lists of floats.
+        :return: The inverse transformed data with NaNs replaced by
+        :code:`self.replace_nan_token`.
+        """
+        X = np.array(X).astype(float)
+        transformed_with_nan = X * self.stds + self.means
+        transformed_with_none = np.where(
+            np.isnan(transformed_with_nan), self.replace_nan_token, transformed_with_nan
+        )
+
+        return transformed_with_none
+
+
+CompScaler = StandardScaler(
+    means=np.array(CompScalerMeans),
+    stds=np.array(CompScalerStds),
+    replace_nan_token=0.0,
+)
+
+# fmt: off
+chemical_symbols = [
+    # 0
+    'X',
+    # 1
+    'H', 'He',
+    # 2
+    'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
+    # 3
+    'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar',
+    # 4
+    'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn',
+    'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr',
+    # 5
+    'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd',
+    'In', 'Sn', 'Sb', 'Te', 'I', 'Xe',
+    # 6
+    'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy',
+    'Ho', 'Er', 'Tm', 'Yb', 'Lu',
+    'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi',
+    'Po', 'At', 'Rn',
+    # 7
+    'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk',
+    'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr',
+    'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc',
+    'Lv', 'Ts', 'Og']
+# fmt: on
+
+CrystalNNFP = CrystalNNFingerprint.from_preset("ops")
+CompFP = ElementProperty.from_preset("magpie")
+
+
+Percentiles = {
+    "mp20": np.array([-3.17562208, -2.82196882, -2.52814761]),
+    "carbon": np.array([-154.527093, -154.45865733, -154.44206825]),
+    "perovskite": np.array([0.43924842, 0.61202443, 0.7364607]),
+}
+
+COV_Cutoffs = {
+    "mp20": {"struc": 0.4, "comp": 10.0},
+    "carbon": {"struc": 0.2, "comp": 4.0},
+    "perovskite": {"struc": 0.2, "comp": 4},
+}
+
+
+def smact_validity(comp, count, use_pauling_test=True, include_alloys=True):
+    elem_symbols = tuple([chemical_symbols[elem] for elem in comp])
+    space = smact.element_dictionary(elem_symbols)
+    smact_elems = [e[1] for e in space.items()]
+    electronegs = [e.pauling_eneg for e in smact_elems]
+    ox_combos = [e.oxidation_states for e in smact_elems]
+    if len(set(elem_symbols)) == 1:
+        return True
+    if include_alloys:
+        is_metal_list = [elem_s in smact.metals for elem_s in elem_symbols]
+        if all(is_metal_list):
+            return True
+
+    threshold = np.max(count)
+    compositions = []
+    for ox_states in itertools.product(*ox_combos):
+        stoichs = [(c,) for c in count]
+        # Test for charge balance
+        cn_e, cn_r = smact.neutral_ratios(
+            ox_states, stoichs=stoichs, threshold=threshold
+        )
+        # Electronegativity test
+        if cn_e:
+            if use_pauling_test:
+                try:
+                    electroneg_OK = pauling_test(ox_states, electronegs)
+                except TypeError:
+                    # if no electronegativity data, assume it is okay
+                    electroneg_OK = True
+            else:
+                electroneg_OK = True
+            if electroneg_OK:
+                for ratio in cn_r:
+                    compositions.append(tuple([elem_symbols, ox_states, ratio]))
+    compositions = [(i[0], i[2]) for i in compositions]
+    compositions = list(set(compositions))
+    if len(compositions) > 0:
+        return True
+    else:
+        return False
+
+
+def structure_validity(crystal, cutoff=0.5):
+    dist_mat = crystal.distance_matrix
+    # Pad diagonal with a large number
+    dist_mat = dist_mat + np.diag(np.ones(dist_mat.shape[0]) * (cutoff + 10.0))
+    if dist_mat.min() < cutoff or crystal.volume < 0.1:
+        return False
+    else:
+        return True
+
+
+class Crystal(object):
+    def __init__(self, crys_array_dict):
+        self.frac_coords = crys_array_dict["frac_coords"]
+        self.atom_types = crys_array_dict["atom_types"]
+        self.lengths = crys_array_dict["lengths"]
+        self.angles = crys_array_dict["angles"]
+        self.dict = crys_array_dict
+
+        self.get_structure()
+        self.get_composition()
+        self.get_validity()
+        self.get_fingerprints()
+
+    def get_structure(self):
+        if min(self.lengths.tolist()) < 0:
+            self.constructed = False
+            self.invalid_reason = "non_positive_lattice"
+        else:
+            try:
+                self.structure = Structure(
+                    lattice=Lattice.from_parameters(
+                        *(self.lengths.tolist() + self.angles.tolist())
+                    ),
+                    species=self.atom_types,
+                    coords=self.frac_coords,
+                    coords_are_cartesian=False,
+                )
+                self.constructed = True
+            except Exception:
+                self.constructed = False
+                self.invalid_reason = "construction_raises_exception"
+            if self.structure.volume < 0.1:
+                self.constructed = False
+                self.invalid_reason = "unrealistically_small_lattice"
+
+    def get_composition(self):
+        elem_counter = Counter(self.atom_types)
+        composition = [
+            (elem, elem_counter[elem]) for elem in sorted(elem_counter.keys())
+        ]
+        elems, counts = list(zip(*composition))
+        counts = np.array(counts)
+        counts = counts / np.gcd.reduce(counts)
+        self.elems = elems
+        self.comps = tuple(counts.astype("int").tolist())
+
+    def get_validity(self):
+        self.comp_valid = smact_validity(self.elems, self.comps)
+        if self.constructed:
+            self.struct_valid = structure_validity(self.structure)
+        else:
+            self.struct_valid = False
+        self.valid = self.comp_valid and self.struct_valid
+
+    def get_fingerprints(self):
+        elem_counter = Counter(self.atom_types)
+        comp = Composition(elem_counter)
+        self.comp_fp = CompFP.featurize(comp)
+        try:
+            site_fps = [
+                CrystalNNFP.featurize(self.structure, i)
+                for i in range(len(self.structure))
+            ]
+        except Exception:
+            # counts crystal as invalid if fingerprint cannot be constructed.
+            self.valid = False
+            self.comp_fp = None
+            self.struct_fp = None
+            return
+        self.struct_fp = np.array(site_fps).mean(axis=0)
+
+
+class RecEval(object):
+    def __init__(self, pred_crys, gt_crys, stol=0.5, angle_tol=10, ltol=0.3):
+        assert len(pred_crys) == len(gt_crys)
+        self.matcher = StructureMatcher(stol=stol, angle_tol=angle_tol, ltol=ltol)
+        self.preds = pred_crys
+        self.gts = gt_crys
+
+    def get_match_rate_and_rms(self):
+        def process_one(pred, gt, is_valid):
+            if not is_valid:
+                return None
+            try:
+                rms_dist = self.matcher.get_rms_dist(pred.structure, gt.structure)
+                rms_dist = None if rms_dist is None else rms_dist[0]
+                return rms_dist
+            except Exception:
+                return None
+
+        validity = [c.valid for c in self.preds]
+
+        rms_dists = []
+        for i in tqdm(range(len(self.preds))):
+            rms_dists.append(process_one(self.preds[i], self.gts[i], validity[i]))
+        rms_dists = np.array(rms_dists)
+        match_rate = sum(rms_dists != None) / len(self.preds)  # noqa: E711
+        mean_rms_dist = rms_dists[rms_dists != None].mean()  # noqa: E711
+        return {"match_rate": match_rate, "rms_dist": mean_rms_dist}
+
+    def get_metrics(self):
+        return self.get_match_rate_and_rms()
+
+
+class GenEval(object):
+    def __init__(
+        self,
+        pred_crys,
+        gt_crys,
+        pred_props,
+        gt_props,
+        eval_model_name=None,
+    ):
+        self.crys = pred_crys
+        self.gt_crys = gt_crys
+        self.pred_props = pred_props
+        self.gt_props = gt_props
+        n_samples = len(pred_props)
+        self.n_samples = n_samples
+        self.eval_model_name = eval_model_name
+
+        valid_crys = [c for c in pred_crys if c.valid]
+        if len(valid_crys) >= n_samples:
+            sampled_indices = np.random.choice(
+                len(valid_crys), n_samples, replace=False
+            )
+            self.valid_samples = [valid_crys[i] for i in sampled_indices]
+        else:
+            raise Exception(
+                f"not enough valid crystals in the predicted set: {len(valid_crys)}/{n_samples}"  # noqa: E501
+            )
+
+    def get_validity(self):
+        comp_valid = np.array([c.comp_valid for c in self.crys]).mean()
+        struct_valid = np.array([c.struct_valid for c in self.crys]).mean()
+        valid = np.array([c.valid for c in self.crys]).mean()
+        return {"comp_valid": comp_valid, "struct_valid": struct_valid, "valid": valid}
+
+    def get_comp_diversity(self):
+        comp_fps = [c.comp_fp for c in self.valid_samples]
+        comp_fps = CompScaler.transform(comp_fps)
+        comp_div = get_fp_pdist(comp_fps)
+        return {"comp_div": comp_div}
+
+    def get_struct_diversity(self):
+        return {"struct_div": get_fp_pdist([c.struct_fp for c in self.valid_samples])}
+
+    def get_density_wdist(self):
+        pred_densities = [c.structure.density for c in self.valid_samples]
+        gt_densities = [c.structure.density for c in self.gt_crys]
+        wdist_density = wasserstein_distance(pred_densities, gt_densities)
+        return {"wdist_density": wdist_density}
+
+    def get_num_elem_wdist(self):
+        pred_nelems = [len(set(c.structure.species)) for c in self.valid_samples]
+        gt_nelems = [len(set(c.structure.species)) for c in self.gt_crys]
+        wdist_num_elems = wasserstein_distance(pred_nelems, gt_nelems)
+        return {"wdist_num_elems": wdist_num_elems}
+
+    def get_prop_wdist(self):
+        if self.pred_props is not None:
+            wdist_prop = wasserstein_distance(self.pred_props, self.gt_props)
+            return {"wdist_prop": wdist_prop}
+        else:
+            return {"wdist_prop": None}
+
+    def get_coverage(self):
+        cutoff_dict = COV_Cutoffs[self.eval_model_name]
+        (cov_metrics_dict, combined_dist_dict) = compute_cov(
+            self.crys,
+            self.gt_crys,
+            struc_cutoff=cutoff_dict["struc"],
+            comp_cutoff=cutoff_dict["comp"],
+        )
+        return cov_metrics_dict
+
+    def get_metrics(self):
+        metrics = {}
+        metrics.update(self.get_validity())
+        metrics.update(self.get_comp_diversity())
+        metrics.update(self.get_struct_diversity())
+        metrics.update(self.get_density_wdist())
+        metrics.update(self.get_num_elem_wdist())
+        metrics.update(self.get_prop_wdist())
+        print(metrics)
+        metrics.update(self.get_coverage())
+        return metrics
+
+
+class OptEval(object):
+    def __init__(self, crys, pred_props, num_opt=100, eval_model_name=None):
+        """
+        crys is a list of length (<step_opt> * <num_opt>), where <num_opt> is the number
+        of different initialization for optimizing crystals,
+        and <step_opt> is the number of saved crystals for each intialzation.
+        default to minimize the property.
+        """
+        step_opt = int(len(crys) / num_opt)
+        self.crys = crys
+        self.pred_props = pred_props
+        self.step_opt = step_opt
+        self.num_opt = num_opt
+        self.eval_model_name = eval_model_name
+
+    def get_success_rate(self):
+        valid_indices = np.array([c.valid for c in self.crys])
+        valid_indices = valid_indices.reshape(self.step_opt, self.num_opt)
+        valid_x, valid_y = valid_indices.nonzero()
+        props = np.ones([self.step_opt, self.num_opt]) * np.inf
+        valid_crys = [c for c in self.crys if c.valid]
+        if len(valid_crys) == 0:
+            sr_5, sr_10, sr_15 = 0, 0, 0
+        else:
+            percentiles = Percentiles[self.eval_model_name]
+            props[valid_x, valid_y] = self.pred_props
+            best_props = props.min(axis=0)
+            sr_5 = (best_props <= percentiles[0]).mean()
+            sr_10 = (best_props <= percentiles[1]).mean()
+            sr_15 = (best_props <= percentiles[2]).mean()
+        return {"SR5": sr_5, "SR10": sr_10, "SR15": sr_15}
+
+    def get_metrics(self):
+        return self.get_success_rate()
