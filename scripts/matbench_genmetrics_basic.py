23faf3884a5aae8bfe9d2c73f2a16b04ff609894
sgbaird <sterling.baird@utah.edu>
2022-07-30 13:49:34 -0600

    Update matbench_genmetrics_basic.py

--- scripts/matbench_genmetrics_basic.py
+++ scripts/matbench_genmetrics_basic.py
@@ -1,17 +1,19 @@
+from mp_time_split.utils.gen import DummyGenerator
 from tqdm import tqdm
 
-from matbench_genmetrics.core import MPTSMetrics
+from matbench_genmetrics.core import MPTSMetrics10
 
-mptm = MPTSMetrics(dummy=False, verbose=False)
+mptm = MPTSMetrics10(dummy=True, verbose=True)
 for fold in tqdm(mptm.folds):
     train_val_inputs = mptm.get_train_and_val_data(fold)
 
-    # dg = DummyGenerator()
-    # dg.fit(train_val_inputs)
-    # gen_structures = dg.gen(n=50)
+    dg = DummyGenerator()
+    dg.fit(train_val_inputs)
+    gen_structures = dg.gen(n=mptm.num_gen)
 
-    gen_structures = train_val_inputs.head(10)
+    # gen_structures = train_val_inputs.head(10)
 
     mptm.evaluate_and_record(fold, gen_structures)
 
 print(mptm.recorded_metrics)
+1 + 1
