1298632733451135b438bff9fe88964dc3660f8d
sgbaird <sterling.baird@utah.edu>
2022-07-30 17:31:35 -0600

    logo

--- docs/conf.py
+++ docs/conf.py
@@ -201,7 +201,7 @@ html_theme_options = {"sidebar_width": "300px", "page_width": "1200px"}
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-# html_logo = "logo.png"
+html_logo = "logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
