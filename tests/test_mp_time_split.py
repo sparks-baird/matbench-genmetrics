8320b5c65a0b3ccc2245cb37705ed5d247375cf1
sgbaird <sterling.baird@utah.edu>
2023-06-17 12:59:56 -0600

    test mp_time_splitter

--- tests/test_mp_time_split.py
+++ tests/test_mp_time_split.py
@@ -6,22 +6,12 @@ from matminer.utils.io import load_dataframe_from_json
 
 from matbench_genmetrics.mp_time_split.splitter import MPTimeSplit, get_data_home
 from matbench_genmetrics.mp_time_split.utils.data import DUMMY_SNAPSHOT_NAME
+from matbench_genmetrics.mp_time_split.utils.split import mp_time_splitter
 
 dummy_data_path = path.join(get_data_home(), DUMMY_SNAPSHOT_NAME)
 
 num_sites = (1, 2)
 elements = ["V"]
-
-
-# def test_data_snapshot():
-#     dummy_expt_df_check = load_dataframe_from_json(dummy_data_path)
-#     mpt = MPTimeSplit(num_sites=(1, 2), elements=["V"])
-#     dummy_expt_df = mpt.fetch_data()
-#     dummy_match = dummy_expt_df.compare(dummy_expt_df_check)
-#     if not dummy_match.empty:
-#         raise ValueError(
-#             f"dummy_expt_df and dummy_expt_df_check unmatched: {dummy_match}"
-#         )
 
 
 @pytest.mark.skipif(sys.version_info < (3, 8), reason="requires Python 3.8+")
@@ -69,6 +59,14 @@ def test_load():
     return data
 
 
+def test_mp_time_splitter():
+    """ """
+    mpt = MPTimeSplit(num_sites=num_sites, elements=elements)
+    data = mpt.load(dummy=True)
+    mp_time_splitter(data, use_trainval_test=True)
+    mp_time_splitter(data, use_trainval_test=False)
+
+
 if __name__ == "__main__":
     # test_data_snapshot()
     test_data_snapshot_one_by_one()
@@ -76,3 +74,14 @@ if __name__ == "__main__":
     train_inputs, val_inputs, train_outputs, val_outputs = test_get_train_and_val_data()
     train_inputs, test_inputs, train_outputs, test_outputs = test_get_test_data()
     data
+
+# %% Code Graveyard
+# def test_data_snapshot():
+#     dummy_expt_df_check = load_dataframe_from_json(dummy_data_path)
+#     mpt = MPTimeSplit(num_sites=(1, 2), elements=["V"])
+#     dummy_expt_df = mpt.fetch_data()
+#     dummy_match = dummy_expt_df.compare(dummy_expt_df_check)
+#     if not dummy_match.empty:
+#         raise ValueError(
+#             f"dummy_expt_df and dummy_expt_df_check unmatched: {dummy_match}"
+#         )
