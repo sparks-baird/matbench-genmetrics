9492f5be7c574efd9198bb2bc1716d35a10c7bd9
sgbaird <sterling.baird@utah.edu>
2023-06-17 14:38:12 -0600

    conf.py black formatting and white background for summary fig

--- docs/conf.py
+++ docs/conf.py
@@ -180,10 +180,7 @@ html_theme = "sphinx_rtd_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    "sidebar_width": "300px",
-    "page_width": "1200px"
-}
+html_theme_options = {"sidebar_width": "300px", "page_width": "1200px"}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
@@ -268,7 +265,13 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-    ("index", "user_guide.tex", "matbench-genmetrics Documentation", "sgbaird", "manual")
+    (
+        "index",
+        "user_guide.tex",
+        "matbench-genmetrics Documentation",
+        "sgbaird",
+        "manual",
+    )
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
